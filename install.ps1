d197dfd0a4272abd29a73ba27d4380a56b10f0e2
tom.flocken <tom.flocken@iteratec.com>
2025-03-03 11:37:33 +0100

    add install script

--- install.ps1
+++ install.ps1
@@ -1,1 +1,10 @@
-echo "hi"
+set-executionpolicy RemoteSigned
+winget install Microsoft.VisualStudioCode
+
+winget install Schniz.fnm
+echo "fnm env --shell powershell | out-string | iex" > $PROFILE
+. $PROFILE
+fnm install 22
+fnm default 22
+
+
