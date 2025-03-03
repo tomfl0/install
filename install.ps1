991167539d279c5a5a8fc61045cf1f2011bceb8d
tom.flocken <tom.flocken@iteratec.com>
2025-03-03 13:13:17 +0100

    create profile directory

--- install.ps1
+++ install.ps1
@@ -1,12 +1,16 @@
 winget install Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
-
 winget install Schniz.fnm
+
 # refresh env
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
+# create powershell profile folder if it doesnt exist yet
+mkdir $(Split-Path -Parent $PROFILE)
+
+# load fnm in powershell
 Write-Output "fnm env --shell powershell | out-string | iex" > $PROFILE
 . $PROFILE
+
+# install node
 fnm install 22
 fnm default 22
-
-
